6868b10861768b6fe9dcbfda2aa717e1a798e400
gusflopes <gusflopes86@gmail.com>
2020-03-20 08:24:04 -0400

    feat: installed dotenv

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+require('dotenv/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const ShortUrl = require('./models/shortUrl');
 const app = express();
 
-mongoose.connect('mongodb://localhost/urlshort', {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
